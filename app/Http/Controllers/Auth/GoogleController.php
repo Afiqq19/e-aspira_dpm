d15ef46f1cf18468f269d8d554069687613dba93
MSyafiq19 <mhdsyafiqsyahmi19@gmail.com>
2026-08-15 22:10:52 +0700

    Temporarily disable email domain restriction for testing

--- app/Http/Controllers/Auth/GoogleController.php
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -26,10 +26,12 @@ class GoogleController extends Controller
         try {
             $googleUser = Socialite::driver('google')->user();
 
-            // KUNCI: Wajib email kampus
+            // KUNCI: Wajib email kampus (Bisa diaktifkan nanti saat aplikasi rilis)
+            /*
             if (!Str::endsWith($googleUser->getEmail(), '@students.polmed.ac.id')) {
                 return redirect()->route('login')->with('error', 'Akses Ditolak! Anda wajib menggunakan akun email kampus (@students.polmed.ac.id). Email biasa tidak diizinkan.');
             }
+            */
 
             // Cek apakah user dengan google_id ini sudah ada
             $user = User::where('google_id', $googleUser->getId())->first();
