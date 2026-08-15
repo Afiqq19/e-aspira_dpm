af81a34634d59fad3d54e44c610cb145194eb1fe
MSyafiq19 <mhdsyafiqsyahmi19@gmail.com>
2026-08-15 21:43:00 +0700

    Fix login layout overlapping issue

--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -85,11 +85,11 @@ new #[Layout('layouts.guest')] class extends Component
                     </a>
                 </div>
 
-                <x-primary-button class="w-full bg-indigo-600 hover:bg-indigo-700 transition duration-150 ease-in-out shadow-md shadow-indigo-500/30 justify-center text-sm py-2.5">
-                    <span wire:loading.remove wire:target="login">{{ __('Masuk Sekarang') }}</span>
+                <x-primary-button type="submit" class="ms-4 bg-indigo-600 hover:bg-indigo-700 transition duration-150 ease-in-out shadow-md shadow-indigo-500/30 min-w-[100px] justify-center">
+                    <span wire:loading.remove wire:target="login">{{ __('Masuk') }}</span>
                     <span wire:loading.flex wire:target="login" class="items-center">
                         <svg class="animate-spin -ml-1 mr-2 h-4 w-4 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle><path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg>
-                        Memproses...
+                        ...
                     </span>
                 </x-primary-button>
             </div>
