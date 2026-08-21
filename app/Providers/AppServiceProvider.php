c6e0c3b302ae44382bbe5f459129a97091ff35bb
MSyafiq19 <mhdsyafiqsyahmi19@gmail.com>
2026-08-21 13:28:51 +0700

    Fix: enforce HTTPS for Livewire endpoints to prevent 405 Method Not Allowed on HTTP->HTTPS proxy redirects

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (str_contains(request()->getHost(), 'xie.my.id')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
+
